7e7c272cd8208ead29124d5737f785920410980f
Agus Mussi <agustinmfacultad@gmail.com>
2026-04-29 02:30:36 -0300

    Agregado de docker compose, para usar mysql

---
+++ app/bd/bd_manager.py
@@ -0,0 +1,62 @@
+import mysql.connector
+from mysql.connector import Error
+
+class DBManager:
+    def __init__(self):
+        self.config = {
+            'host': 'localhost',          # Al estar en Docker pero mapear puertos, usas localhost
+            'user': 'usuario_rpg',        # Lo definiste en tu docker-compose
+            'password': 'proyecto_rpg',   # Lo definiste en tu docker-compose
+            'database': 'rpg_db'          # Lo definiste en tu docker-compose
+        }
+
+    def conectar(self):
+        try:
+            conexion = mysql.connector.connect(**self.config)
+            if conexion.is_connected():
+                return conexion
+        except Error as e:
+            print(f"Error al conectar: {e}")
+            return None
+
+    def guardar_personaje(self, personaje):
+        conexion = self.conectar()
+        if conexion:
+            cursor = conexion.cursor()
+            # Esta es la parte de "SQL puro" que querías ver
+            query = """
+            INSERT INTO personaje 
+            (nombre, clase, nivel, experiencia, vida_max, defensa, ataque, agilidad, defensa_magica, ataque_magico, magia_max)
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """
+            valores = (
+                personaje.nombre, 
+                personaje.clase, 
+                personaje.nivel,
+                personaje.experiencia_actual,
+                personaje.vida,
+                personaje.defensa,
+                personaje.ataque,
+                personaje.agilidad,
+                personaje.defensa_magica,
+                personaje.ataque_magico,
+                personaje.magia,
+            )
+            
+            cursor.execute(query, valores)
+            conexion.commit() # ¡Importante! Sin esto no se guardan los cambios
+            cursor.close()
+            conexion.close()
+            print(f"¡{personaje.nombre} guardado en la DB!")
+
+
+    def cargar_personaje(self, nombre):
+        conexion=self.conectar()
+        cursor = conexion.cursor()
+
+        query = "SELECT nombre, clase, nivel FROM personaje WHERE nombre= %s"
+        cursor.execute(query, (nombre, ))
+
+        resultado = cursor.fetchone()
+
+        print(resultado)
---
+++ pruebasDB.py
@@ -0,0 +1,27 @@
+from app.entidades.berserker import Berserker
+from app.entidades.mago import Mago
+from app.entidades.strider import Strider
+from app.bd.bd_manager import  DBManager
+
+def test_db():
+    db = DBManager()
+    """
+    # 1. Creamos los personajes (instancias de tus clases)
+    conan = Berserker(nombre="Conan")
+    gandalf = Mago(nombre="Gandalf")
+    legolas = Strider(nombre="Legolas")
+
+    # 2. Guardamos en la base de datos
+    print("--- Guardando personajes ---")
+    db.guardar_personaje(conan)
+    db.guardar_personaje(gandalf)
+    db.guardar_personaje(legolas)
+    """
+    # 3. Probamos la carga
+    print("\n--- Consultando la base de datos ---")
+    db.cargar_personaje("Conan")
+    db.cargar_personaje("Gandalf")
+    db.cargar_personaje("Legolas")
+
+if __name__ == "__main__":
+    test_db()
